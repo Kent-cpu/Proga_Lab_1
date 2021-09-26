8d75d130ab783f3ff1bfc01140a5b05bc2829211
Руслан Мирзаев <ruslanmirzaev16621@gmail.com>
2021-09-26 23:44:40 +0700

    Ready

---
+++ Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace Prog_lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.расчитатьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.очиститьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bottomBorderField = new System.Windows.Forms.TextBox();
+            this.accuracyField = new System.Windows.Forms.TextBox();
+            this.topBorderField = new System.Windows.Forms.TextBox();
+            this.formulaField = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.label5 = new System.Windows.Forms.Label();
+            this.answerField = new System.Windows.Forms.TextBox();
+            this.errorMessage = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.расчитатьToolStripMenuItem,
+            this.очиститьToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // расчитатьToolStripMenuItem
+            // 
+            this.расчитатьToolStripMenuItem.Name = "расчитатьToolStripMenuItem";
+            this.расчитатьToolStripMenuItem.Size = new System.Drawing.Size(91, 26);
+            this.расчитатьToolStripMenuItem.Text = "Расчитать";
+            this.расчитатьToolStripMenuItem.Click += new System.EventHandler(this.расчитатьToolStripMenuItem_Click);
+            // 
+            // очиститьToolStripMenuItem
+            // 
+            this.очиститьToolStripMenuItem.Name = "очиститьToolStripMenuItem";
+            this.очиститьToolStripMenuItem.Size = new System.Drawing.Size(87, 26);
+            this.очиститьToolStripMenuItem.Text = "Очистить";
+            this.очиститьToolStripMenuItem.Click += new System.EventHandler(this.очиститьToolStripMenuItem_Click);
+            // 
+            // bottomBorderField
+            // 
+            this.bottomBorderField.Location = new System.Drawing.Point(22, 52);
+            this.bottomBorderField.Name = "bottomBorderField";
+            this.bottomBorderField.Size = new System.Drawing.Size(110, 22);
+            this.bottomBorderField.TabIndex = 1;
+            // 
+            // accuracyField
+            // 
+            this.accuracyField.Location = new System.Drawing.Point(303, 52);
+            this.accuracyField.Name = "accuracyField";
+            this.accuracyField.Size = new System.Drawing.Size(110, 22);
+            this.accuracyField.TabIndex = 2;
+            // 
+            // topBorderField
+            // 
+            this.topBorderField.Location = new System.Drawing.Point(159, 52);
+            this.topBorderField.Name = "topBorderField";
+            this.topBorderField.Size = new System.Drawing.Size(110, 22);
+            this.topBorderField.TabIndex = 3;
+            // 
+            // formulaField
+            // 
+            this.formulaField.Location = new System.Drawing.Point(435, 52);
+            this.formulaField.Name = "formulaField";
+            this.formulaField.Size = new System.Drawing.Size(240, 22);
+            this.formulaField.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "a";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(156, 32);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "b";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(300, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(16, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "e";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(432, 32);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 17);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "f(x)";
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(13, 205);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(761, 462);
+            this.zedGraphControl1.TabIndex = 9;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label5.Location = new System.Drawing.Point(19, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 17);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Ответ";
+            // 
+            // answerField
+            // 
+            this.answerField.Location = new System.Drawing.Point(22, 133);
+            this.answerField.Name = "answerField";
+            this.answerField.Size = new System.Drawing.Size(203, 22);
+            this.answerField.TabIndex = 11;
+            // 
+            // errorMessage
+            // 
+            this.errorMessage.AutoSize = true;
+            this.errorMessage.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.errorMessage.ForeColor = System.Drawing.Color.Red;
+            this.errorMessage.Location = new System.Drawing.Point(343, 102);
+            this.errorMessage.Name = "errorMessage";
+            this.errorMessage.Size = new System.Drawing.Size(0, 17);
+            this.errorMessage.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 680);
+            this.Controls.Add(this.errorMessage);
+            this.Controls.Add(this.answerField);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.formulaField);
+            this.Controls.Add(this.topBorderField);
+            this.Controls.Add(this.accuracyField);
+            this.Controls.Add(this.bottomBorderField);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem расчитатьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem очиститьToolStripMenuItem;
+        private System.Windows.Forms.TextBox bottomBorderField;
+        private System.Windows.Forms.TextBox accuracyField;
+        private System.Windows.Forms.TextBox topBorderField;
+        private System.Windows.Forms.TextBox formulaField;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox answerField;
+        private System.Windows.Forms.Label errorMessage;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,163 @@
+﻿using MathNet.Symbolics;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace Prog_lab2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            zedGraphControl1.GraphPane.Title.Text = "f(x)";
+        }
+
+        private double calculateValuePoint(double value)
+        {
+            double result = 0;
+            try
+            {
+                var variables = new Dictionary<string, FloatingPoint> { { "x", value } };
+                var formula = Infix.ParseOrThrow(formulaField.Text);
+                result = Evaluate.Evaluate(variables, formula).RealValue;
+            }catch(Exception ex)
+            {
+                errorMessage.Text = ex.Message;
+                zedGraphControl1.GraphPane.CurveList.Clear();
+                answerField.Text = "";
+            }
+
+
+            return result;
+
+        }
+
+        private void расчитатьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            errorMessage.Text = "";
+            if (double.TryParse(bottomBorderField.Text, out double bottomBorder) && double.TryParse(topBorderField.Text, out double topBorder)
+                && double.TryParse(accuracyField.Text, out double accuracy)){
+
+                if(bottomBorder >= topBorder)
+                {
+                    errorMessage.Text = "Значение a должно быть меньше значения b";
+                    return;
+                }else if(accuracy <= 0)
+                {
+                    errorMessage.Text = "Точность должна быть больше 0";
+                    return;
+                }
+
+               
+                int decimalPlaces = 0;
+
+
+                for(double i = accuracy; i < 1; i *= 10)
+                {
+                    ++decimalPlaces;
+                }
+
+                
+
+
+         
+                PointPairList list = new PointPairList();
+
+                GraphPane mypane = zedGraphControl1.GraphPane;
+                mypane.CurveList.Clear();
+                
+
+
+                mypane.XAxis.MajorGrid.IsVisible = true;
+                mypane.XAxis.MinorGrid.IsVisible = true;
+                mypane.YAxis.MajorGrid.IsVisible = true;
+
+                mypane.XAxis.Scale.Min = bottomBorder;
+                mypane.XAxis.Scale.Max = topBorder;
+
+                // По оси Y установим автоматический подбор масштаба
+                mypane.YAxis.Scale.MinAuto = true;
+                mypane.YAxis.Scale.MaxAuto = true;
+                mypane.IsBoundedRanges = true;
+
+                for (double x = bottomBorder; x < topBorder; x += accuracy)
+                {
+                    try
+                    {
+                        list.Add(x, calculateValuePoint(x));
+                    }
+                    catch (Exception ex)
+                    {
+                        errorMessage.Text = "Слишком большая точность или интервал";
+                    }
+                   
+                }
+
+               
+                double minPoint = 0;
+
+
+                while (topBorder - bottomBorder >= accuracy)
+                {
+                    double center = Math.Round((topBorder + bottomBorder) / 2, decimalPlaces);
+                    double left = center - accuracy;
+                    double right = center + accuracy;
+
+
+                    if (Math.Round(calculateValuePoint(left), decimalPlaces) < Math.Round(calculateValuePoint(right), 2))
+                    {
+                        topBorder = center;
+                    }
+                    else
+                    {
+                        bottomBorder = center;
+                    }
+
+
+                    minPoint = center;
+                }
+               
+
+                LineItem myCurve = mypane.AddCurve("f(x)", list, Color.Blue, SymbolType.None);
+                LineItem minPointCurve = mypane.AddCurve("Min point", new double[] { minPoint }, new double[] { calculateValuePoint(minPoint) }, Color.DarkRed, SymbolType.Circle);
+                minPointCurve.Symbol.Fill.Type = FillType.Solid;
+
+
+                // Вызываем метод AxisChange (), чтобы обновить данные об осях.
+                // В противном случае на рисунке будет показана только часть графика,
+                // которая умещается в интервалы по осям, установленные по умолчанию
+                zedGraphControl1.AxisChange();
+                zedGraphControl1.Invalidate();
+                answerField.Text = "x: " + minPoint.ToString() + " y: " + calculateValuePoint(minPoint).ToString();
+            }
+            else
+            {
+                errorMessage.Text = "Неверные входные данные";
+            }
+       
+
+
+        }
+
+        private void очиститьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            foreach (Control element in Controls)
+            {
+                if (element is TextBox)
+                {
+                    ((TextBox) element).Text = null;
+                }
+                
+            }
+                
+
+        }
+
+
+
+
+    }
+}
